911f254895352fe04058feab0c18191052c8553b
Diego Achury <achurycompany@gmail.com>
2013-07-06 13:21:32 -0500

    creando mi primera clase en ruby

--- Capitulo1/hola_mundo.rb
+++ Capitulo1/hola_mundo.rb
@@ -1,1 +1,12 @@
-puts "Hola Mundo"
+#puts "Hola Mundo"
+class HolaMundo
+	def initialize()
+	end
+	def saluda()
+		puts "hola Mundo"		
+	end
+end
+
+objeto = HolaMundo.new()
+objeto.saluda
+gets() #deja caturado en pantalla solo para usuarios windows
