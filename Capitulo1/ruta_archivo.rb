2935bb9497ca164b6d903ca14b28beb68cdb392c
Diego Achury <achurycompany@gmail.com>
2013-07-07 15:36:46 -0500

    modificando el archivo ruta para que pueda leer bien los archivos

--- Capitulo1/ruta_archivo.rb
+++ Capitulo1/ruta_archivo.rb
@@ -1,8 +1,18 @@
+#317 770 38 54
+
 require 'find'
  
+ ruta_datos= '/Users/DiegoAchury/Documents/rails_projects/ruby/2008/ABRIL/ABRIL 01/Cartera/123123.tif'
 # muestra la ruta ./
 # que es el directorio de Ruby
 Find.find('/Users/DiegoAchury/Documents/rails_projects/ruby') do |f|
+    if ruta_datos != f
+        
+    else
+        puts "#{f} archivo encontrado"
+    end 
+    
+
   # formatea el resultado
-  puts "#{f}"
+ # puts "#{f}"
 end
